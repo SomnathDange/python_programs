463068c2d64b95b7f28302d622f1296f151d5257
SDemo <dange.somanath@gmail.com>
2018-03-17 12:47:53 +0530

    Update reverseString.py

--- reverseString.py
+++ reverseString.py
@@ -4,3 +4,10 @@ def reverse(name):
     else:
         return reverse(name[1:]) + name[0] 
 print reverse("school")
+
+name="Tata Consultancy"
+str = ""
+for i in name:
+    str=i + str
+print str
+
