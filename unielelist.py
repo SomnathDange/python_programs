5a039ab7f5ee9695912918c0426ebb7240a711f3
SDemo <dange.somanath@gmail.com>
2018-05-16 19:49:20 +0530

    Update unielelist.py

--- unielelist.py
+++ unielelist.py
@@ -2,3 +2,10 @@ no = [3,6,9,12,89,9,6,12]
 uniq_set = set(no)
 uniq_list = list(uniq_set)
 print uniq_list
+
+########### EMPTY LIST ########
+no = []
+if len(no) == 0:
+    print "Empty"
+else:
+    print "Not Empty"
