6185b86c480dfd79e77022dded6cc4e6f9f8da46
SDemo <dange.somanath@gmail.com>
2020-02-08 13:52:02 +0530

    Update generic.py

--- generic.py
+++ generic.py
@@ -14,3 +14,11 @@ for i in range(1,len(numbers)):
 2
 56
  
+##Get unique values from a list
+numbers = [34,2,8,56,91,7,2,8,11,55,91]
+uniq_list = []
+for i in numbers:
+    if i not in uniq_list:
+       uniq_list.append(i)
+print uniq_list
+#Output = [34, 2, 8, 56, 91, 7, 11, 55]
